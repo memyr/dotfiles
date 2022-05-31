c6adc6158b3aa12915be65deb057fbbe2df8c38f
memyr <87771062+memyr@users.noreply.github.com>
2022-05-31 17:39:55 -0500

    turned off typechecking in pyright (issues with stubs), fixed null-ls formatting issues

--- .config/nvim/lua/plugins/lsp/lsp-installer.lua
+++ .config/nvim/lua/plugins/lsp/lsp-installer.lua
@@ -1,58 +1,62 @@
 local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
 if not status_ok then
-	return
+    return
 end
 
 local status_ok, lspconfig = pcall(require, "lspconfig")
 if not status_ok then
-	return
+    return
 end
 
-local servers = { "bashls", "pyright", "sumneko_lua", "texlab", "grammarly" }
+local servers = { "bashls", "sumneko_lua", "texlab", "grammarly", "pyright" }
 lsp_installer.setup({
-	ensure_installed = servers,
-	automatic_installation = true,
+    ensure_installed = servers,
+    automatic_installation = true,
 })
 
 for _, server in ipairs(servers) do
-	if server == "sumneko_lua" then
-		lspconfig[server].setup({
-			on_attach = require("plugins.lsp.handlers").on_attach,
-			capabilities = require("plugins.lsp.handlers").capabilities,
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" },
-					},
-				},
-			},
-		})
-	elseif server == "pyright" then
-		lspconfig[server].setup({
-			on_attach = require("plugins.lsp.handlers").on_attach,
-			capabilities = require("plugins.lsp.handlers").capabilities,
-			settings = {
-				python = {
-					pythonPath = "/usr/local/anaconda3/bin/python",
-				},
-			},
-		})
-	elseif server == "texlab" then
-		lspconfig[server].setup({
-			on_attach = require("plugins.lsp.handlers").on_attach,
-			capabilities = require("plugins.lsp.handlers").capabilities,
-			settings = {
-				texlab = {
-					latexindent = {
-						modifyLineBreaks = true,
-					},
-				},
-			},
-		})
-	else
-		lspconfig[server].setup({
-			on_attach = require("plugins.lsp.handlers").on_attach,
-			capabilities = require("plugins.lsp.handlers").capabilities,
-		})
-	end
+    if server == "sumneko_lua" then
+        lspconfig[server].setup({
+            on_attach = require("plugins.lsp.handlers").on_attach,
+            capabilities = require("plugins.lsp.handlers").capabilities,
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { "vim" },
+                    },
+                },
+            },
+        })
+    elseif server == "pyright" then
+        lspconfig[server].setup({
+            on_attach = require("plugins.lsp.handlers").on_attach,
+            capabilities = require("plugins.lsp.handlers").capabilities,
+            settings = {
+                python = {
+                    pythonPath = "/usr/local/anaconda3/bin/python",
+                    -- too many issues with stubs (e.g., matplotlib)
+                    analysis = {
+                        typeCheckingMode = "off",
+                    },
+                },
+            },
+        })
+    elseif server == "texlab" then
+        lspconfig[server].setup({
+            on_attach = require("plugins.lsp.handlers").on_attach,
+            capabilities = require("plugins.lsp.handlers").capabilities,
+            settings = {
+                texlab = {
+                    latexindent = {
+                        modifyLineBreaks = true,
+                    },
+                },
+            },
+        })
+    else
+        lspconfig[server].setup({
+            on_attach = require("plugins.lsp.handlers").on_attach,
+            capabilities = require("plugins.lsp.handlers").capabilities,
+        })
+    end
 end
--- .config/nvim/lua/plugins/lsp/null-ls.lua
+++ .config/nvim/lua/plugins/lsp/null-ls.lua
@@ -1,40 +1,28 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-    return
-end
-
-local lsp_formatting = function(bufnr)
-    vim.lsp.buf.format({
-        filter = function(clients)
-            -- filter out clients that you don't want to use
-            return vim.tbl_filter(function(client)
-                return client.name ~= "texlab"
-            end, clients)
-        end,
-        bufnr = bufnr,
-    })
+	return
 end
 
 local lsp_augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.stylua.with({ extra_args = { "--column-width", "200" } }),
-        null_ls.builtins.formatting.black.with({ extra_args = { "--fast" } }),
-        -- null_ls.builtins.formatting.latexindent.with({ extra_args = { "-m" } }),
-    },
+	sources = {
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.stylua.with({ extra_args = { "--column-width", "200" } }),
+		null_ls.builtins.formatting.black.with({ extra_args = { "--fast" } }),
+		-- null_ls.builtins.formatting.latexindent.with({ extra_args = { "-m" } }),
+	},
 
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = lsp_augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = lsp_augroup,
-                buffer = bufnr,
-                callback = function()
-                    lsp_formatting(bufnr)
-                end,
-            })
-        end
-    end,
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = lsp_augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = lsp_augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
 })
